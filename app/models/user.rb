af22cf51a95c57cbf0e9f8b053f51b0824e44dc9
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-19 16:49:57 +0200

    Install devise, change old User model to a devise generated one

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
-    has_many :events, foreign_key: "host_id"
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable    
+  
+  has_many :events, foreign_key: "host_id"
 end
