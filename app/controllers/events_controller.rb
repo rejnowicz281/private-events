e1b0b5a12b3f6c3700190e995a862bf2ec06b937
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-20 13:47:06 +0200

    Create Events controller

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,28 @@
+class EventsController < ApplicationController
+    def index
+        @events = Event.all
+    end
+
+    def show
+        @event = Event.find(params[:id])
+    end
+
+    def new
+        @event = Event.new
+    end
+
+    def create
+        @event = current_user.events.build(event_params)
+
+        if @event.save
+            redirect_to @event
+        else
+            render :new, status: :unprocessable_entity
+        end
+    end
+
+    private
+    def event_params
+        params.require(:event).permit(:name, :description, :date, :location, :host_id)
+    end
+end
