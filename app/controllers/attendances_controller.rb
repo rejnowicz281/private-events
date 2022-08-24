222edf41f73c0e660f5512fe2fb00af20c1958f0
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-24 17:59:40 +0200

    Create button for the user to attend an event

--- app/controllers/attendances_controller.rb
+++ app/controllers/attendances_controller.rb
@@ -1,4 +1,7 @@
 class AttendancesController < ApplicationController
     def create
+        @attendance = current_user.attendances.create(attended_event_id: params[:event_id])
+
+        redirect_to event_path(params[:event_id])
     end
 end
