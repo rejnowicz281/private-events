3bc54358324ee8ea6694d754da11d76224a66e70
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-24 14:01:29 +0200

    Create Attendance controller

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   resources :events, only: [:index, :show, :new, :create]
+
+  resources :attendances, only: [:create]
   # Defines the root path route ("/")
   root "events#index"
 end
