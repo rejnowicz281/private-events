1ce9b0df9b320957ac569a6dcb765c1afcc47d96
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-26 22:13:26 +0200

    Make it so that invites controller always looks for event's host invites

--- app/controllers/invites_controller.rb
+++ app/controllers/invites_controller.rb
@@ -1,12 +1,14 @@
 class InvitesController < ApplicationController
     def create
-        @invite = current_user.invites.create(event_id: params[:event_id], invited_id: params[:invited_id])
+        event_host = Event.find(params[:event_id]).host
+        @invite = event_host.invites.create(event_id: params[:event_id], invited_id: params[:invited_id])
 
         redirect_to event_path(params[:event_id])
     end
 
     def destroy
-        @invite = current_user.invites.find_by(event_id: params[:event_id], invited_id: params[:invited_id])
+        event_host = Event.find(params[:event_id]).host
+        @invite = event_host.invites.find_by(event_id: params[:event_id], invited_id: params[:invited_id])
 
         @invite.destroy
         redirect_to event_path(params[:event_id]), status: 303
