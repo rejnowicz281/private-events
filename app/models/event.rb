73b27c25b4a3687f1c3e7b5032d83e74afbb1721
Alan Rejnowicz <alanrejnowicz281@gmail.com>
2022-08-27 00:14:02 +0200

    Add validation for corrent date when creating event

--- app/models/event.rb
+++ app/models/event.rb
@@ -9,7 +9,14 @@ class Event < ApplicationRecord
     validates :name, presence: true
     validates :description, presence: true
     validates :location, presence: true
+    validate :date_must_be_valid
 
     scope :past, -> { where('date < ?', DateTime.now) }
     scope :upcoming, -> { where(date: DateTime.now..) }
+
+    def date_must_be_valid
+        if date < DateTime.now
+            errors.add(:date, "must be valid")
+        end
+    end
 end
